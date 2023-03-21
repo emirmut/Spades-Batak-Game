d36ad3e6a9a09544f0f3304af9d6022bae0365df
emirmut <126797950+emirmut@users.noreply.github.com>
2023-03-22 02:23:59 +0300

    Add files via upload
    Objects has been created. 
    Some part of Card and Deck objects are done. 
    Most part of Node and LinkedList objects are done

---
+++ SpadesProject/src/main/java/Deck.java
@@ -0,0 +1,30 @@
+/**
+ * A deck consists of 52 unique card objects. 13 spades, 13 diamonds, 13 clubs and 13 hearts.
+ * It can shuffle and deal the deck. 1 is ace, 11 is Jack, 12 is Queen, 13 is King.
+ */
+public class Deck {
+    private LinkedList deck; // linked list of 52 cards
+
+    /**
+     * Deck constructor creates a new linked list of 52 unique cards
+     * and shuffles them in a random order.
+     */
+    public Deck() { // Constructor
+
+    }
+
+    /**
+     * shuffles the order of the deck
+     */
+    void shuffle() {
+
+    }
+
+    /**
+     * Prints every card of the deck
+     * @return all cards in the deck
+     */
+    public String toString() {
+
+    }
+}
