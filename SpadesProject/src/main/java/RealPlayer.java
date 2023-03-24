0b70a26ef78ac92cebff2559965cf99e30537a0f
emirmut <126797950+emirmut@users.noreply.github.com>
2023-03-24 22:07:01 +0300

    Update RealPlayer.java
    RealPlayer class has been complete.

--- SpadesProject/src/main/java/RealPlayer.java
+++ SpadesProject/src/main/java/RealPlayer.java
@@ -1,3 +1,11 @@
-public class RealPlayer {
+/**
+ * Human is a player, who plays the Spades game, having a name and a hand.
+ */
+public class RealPlayer extends Player {
+  /**
+   * calls Player constructor using super method without parameters
+   */
+    public RealPlayer() { // Constructor
 
+    }
 }
