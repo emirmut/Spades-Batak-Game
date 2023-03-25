c8c919148870231cfa5158069c61977a680129cb
emirmut <126797950+emirmut@users.noreply.github.com>
2023-03-26 02:20:51 +0300

    Update Bot.java
    Bot class has been complete.

--- SpadesProject/src/main/java/Bot.java
+++ SpadesProject/src/main/java/Bot.java
@@ -1,3 +1,62 @@
-public class Bot {
+/**
+ * The bot has a hand and four different playCard methods
+ * based on the number of cards in the current middle pile.
+ * It determines the most logical choice to play in that situation
+ * based on PlayedCards class and current cards in its hand.
+ */
+public class Bot extends Player {
+    /**
+     * calls Player constructor using super method without parameters
+     */
+    public Bot() { // Constructor
 
+    }
+
+    /**
+     * this method allows Bot to play when the middle pile is empty
+     * @return card from bot's hand
+     */
+    private Card playCard1() {
+
+    }
+
+    /**
+     * this method allows Bot to play when the middle pile has 1 card
+     * @param card card led by the first player, first card in the middle pile
+     * @return card from bot's hand
+     */
+    private Card playCard2(Card card) {
+
+    }
+
+    /**
+     * this method allows Bot to play when the middle pile has 2 cards
+     * @param fcimp card led by the first player, fcimp stands for first card in the middle pile
+     * @param scimp card played second in the round, scimp stands for second card in the middle pile
+     * @return card from bot's hand
+     */
+    private Card playCard3(Card fcimp, Card scimp) {
+
+    }
+
+    /**
+     * this method allows Bot to play when the middle pile has 3 cards
+     * @param fcimp card led by the first player, fcimp stands for first card in the middle pile
+     * @param scimp card played second in the round, scimp stands for second card in the middle pile
+     * @param tcimp card played third in the round, tcimp stands for third card in the middle pile
+     * @return card from bot's hand
+     */
+    private Card playCard4(Card fcimp, Card scimp, Card tcimp) {
+
+    }
+
+    /**
+     * takes in the cards already played and plays the most logical card to play according to the other player
+     * @param l cards that are already played (the middle pile)
+     * @return bot card played
+     */
+    Card playCardMain(LinkedList l) {
+
+    }
 }
+
