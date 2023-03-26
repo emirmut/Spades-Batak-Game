821303c2d2475cbabeb39f962af83761687f0472
emirmut <126797950+emirmut@users.noreply.github.com>
2023-03-26 14:51:49 +0300

    Update Bot.java

--- SpadesProject/src/main/java/Bot.java
+++ SpadesProject/src/main/java/Bot.java
@@ -3,6 +3,7 @@
  * based on the number of cards in the current middle pile.
  * It determines the most logical choice to play in that situation
  * based on PlayedCards class and current cards in its hand.
+ * It also has a method that evaluates which playCard method should be implemented.
  */
 public class Bot extends Player {
     /**
